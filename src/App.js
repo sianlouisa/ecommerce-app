99474dff8e24fce4b6807bfbf3fe473f903de334
SianLouisa <sianlouisa@hotmail.com>
2019-09-26 17:40:25 +0200

    added svg logo and header component with links to shop and contact

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import Shop from './pages/Shop';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Shop from "./pages/Shop";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={Shop} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/shop" component={Shop} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
