44bdda80ed5d40b81aeef7c3280b9f296eeae5d1
SianLouisa <sianlouisa@hotmail.com>
2019-09-26 14:32:16 +0200

    added Shop page

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SHOP_DATA from "../data/shop.js";
+import CollectionPreview from "../components/CollectionPreview.jsx";
+
+class Shop extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			collections: SHOP_DATA,
+		};
+	}
+	render() {
+		const { collections } = this.state;
+		return (
+			<div className="shop-page">
+				{collections.map(({ id, ...otherCollectionProps }) => (
+					<CollectionPreview key={id} {...otherCollectionProps} />
+				))}
+			</div>
+		);
+	}
+}
+
+export default Shop;
