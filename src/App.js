ed29282afde3f4abfe65be3e0cd1eb7b97efdd2e
SianLouisa <sianlouisa@hotmail.com>
2019-09-26 14:29:15 +0200

    routing to shop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Shop from './pages/Shop';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
     </div>
   );
