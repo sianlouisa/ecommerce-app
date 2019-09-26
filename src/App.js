573dffcb0dbf60882292c598476f89015069d3ed
SianLouisa <sianlouisa@hotmail.com>
2019-09-26 12:08:41 +0200

    added withRouter to navigate menu-items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
