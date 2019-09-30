a807cd3886880233dabe00831a65d43c06fe5d32
SianLouisa <sianlouisa@hotmail.com>
2019-09-30 17:07:32 +0200

    implement createUser reducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase/firebase.utils";
 import { ReactComponent as Logo } from "../assets/crown.svg";
+import { connect } from "react-redux";
 
 const Header = ({ currentUser }) => (
 	<div className="header">
@@ -28,4 +29,8 @@ const Header = ({ currentUser }) => (
 	</div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
