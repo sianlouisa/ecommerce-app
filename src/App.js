45727fafc5b91d9c1f2d754af75a7aa2b2b9532e
SianLouisa <sianlouisa@hotmail.com>
2019-09-30 17:14:29 +0200

    added user.types file to store action types object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
@@ -37,24 +37,33 @@ class App extends React.Component {
 	}
 
 	render() {
+		const { currentUser } = this.props;
 		return (
 			<div>
 				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
-					<Route path="/signin" component={SignInPage} />
+					<Route
+						exact
+						path="/signin"
+						render={() => (currentUser ? <Redirect to="/" /> : <SignInPage />)}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = ({ user }) => ({
+	currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = dispatch => ({
 	setCurrentUser: user => dispatch(setCurrentUser(user)),
 });
 
 export default connect(
-	null,
+	mapStateToProps,
 	mapDispatchToProps,
 )(App);
