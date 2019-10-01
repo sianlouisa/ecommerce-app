b40112bf3c3ba067f7a7fd9bdd58321d51f43174
SianLouisa <sianlouisa@hotmail.com>
2019-10-01 15:52:18 +0200

    added inverted prop and styling to customer button for use instead of collectionitem

--- src/components/custom-button.jsx
+++ src/components/custom-button.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+	children,
+	isGoogleSignIn,
+	inverted,
+	...otherProps
+}) => (
 	<button
-		className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+		className={`{${inverted ? "inverted" : ""}} 
+		${isGoogleSignIn ? "google-sign-in" : ""} 
+		custom-button`}
 		{...otherProps}
 	>
 		{children}
