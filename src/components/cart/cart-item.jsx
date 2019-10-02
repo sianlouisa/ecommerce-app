34629befebe74b49a85afc6d790b5435cc5947be
SianLouisa <sianlouisa@hotmail.com>
2019-10-02 10:46:01 +0200

    added cart item component and connected cart dropdown with cart items reducer

---
+++ src/components/cart/cart-item.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+	<div className="cart-item">
+		<img src={imageUrl} alt={name} />
+		<div className="item-details">
+			<span className="name">{name}</span>
+			<span className="price">
+				{quantity} x £{price}
+			</span>
+		</div>
+	</div>
+);
+
+export default CartItem;
--- src/components/collections/collection-item.jsx
+++ src/components/collections/collection-item.jsx
@@ -10,7 +10,7 @@ const CollectionItem = ({ item, addItem }) => {
 			<div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
 			<div className="collection-footer">
 				<span className="name">{name}</span>
-				<span className="price">{price}</span>
+				<span className="price">£{price}</span>
 			</div>
 			<CustomButton inverted onClick={() => addItem(item)}>
 				ADD TO CART
