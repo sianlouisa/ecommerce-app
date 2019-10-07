c91c9081145f0dda57b44e765fad372ceb493d60
SianLouisa <sianlouisa@hotmail.com>
2019-10-07 12:43:54 +0200

    persist redux cart state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+// use local storage as default storage, can also use sessionStorage
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+	// point inside reducer object start storing
+	key: "root",
+	storage,
+	// reducers to store
+	whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./root-reducer";
+import { persistStore } from "redux-persist";
 
 const middlewares = [];
 
@@ -14,6 +15,8 @@ if (process.env.NODE_ENV === "development") {
 	middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
